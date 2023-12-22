b1acb706ac36d2cec356720e571849f11b068118
Meraj Faizan <mdmaraj1500@gmail.com>
2023-12-22 16:22:10 +0600

    data delete and update endpoint added

--- index.js
+++ index.js
@@ -61,7 +61,99 @@ async function run() {
       res.send(result);
     });
 
+    // get all tasks of a user
+    app.get("/tasks/user/:email", async (req, res) => {
+      try {
+        const email = req.params.email;
+        const query = { email: email };
+
+        // Assuming userCollection is a MongoDB collection
+        const user = await userCollection.findOne(query);
+
+        if (!user) {
+          return res
+            .status(404)
+            .send({ success: false, message: "User not found" });
+        }
+
+        const tasks = user.tasks || []; // Use an empty array if tasks is null
+        res.send(tasks);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+        res
+          .status(500)
+          .send({ success: false, message: "Internal server error" });
+      }
+    });
+
+    app.delete('/tasks/:email/:taskId', async (req, res) => {
+      const userEmail = req.params.email;
+      const taskId = req.params.taskId;
     
+      try {
+        const result = await userCollection.updateOne(
+          { email: userEmail },
+          { $pull: { tasks: { tid: taskId } } }
+        );
+    
+        if (result.modifiedCount === 1) {
+          res.status(200).json({ success: true, message: 'Task deleted successfully' });
+        } else {
+          res.status(404).json({ success: false, message: 'Task not found' });
+        }
+      } catch (error) {
+        console.error('Error deleting task:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+      }
+    });
+
+    app.get('/tasks/user/:email/:taskId', async (req, res) => {
+      const userEmail = req.params.email;
+      const taskId = req.params.taskId;
+    
+      try {
+        const user = await userCollection.findOne({ email: userEmail });
+    
+        if (!user) {
+          return res.status(404).json({ success: false, message: 'User not found' });
+        }
+    
+        const taskToEdit = user.tasks.find((task) => task.tid === taskId);
+    
+        if (!taskToEdit) {
+          return res.status(404).json({ success: false, message: 'Task not found' });
+        }
+    
+        res.status(200).json({ success: true, task: taskToEdit });
+      } catch (error) {
+        console.error('Error fetching task:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+      }
+    });
+
+
+    app.put('/tasks/user/:email/:taskId', async (req, res) => {
+      const userEmail = req.params.email;
+      const taskId = req.params.taskId;
+      const updatedTaskData = req.body;
+    
+      try {
+        const result = await userCollection.updateOne(
+          { email: userEmail, 'tasks.tid': taskId },
+          { $set: { 'tasks.$': updatedTaskData } }
+        );
+    
+        if (result.modifiedCount === 1) {
+          res.status(200).json({ success: true, message: 'Task updated successfully' });
+        } else {
+          res.status(404).json({ success: false, message: 'Task not found or not updated' });
+        }
+      } catch (error) {
+        console.error('Error updating task:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+      }
+    });
+
   } finally {
     // await client.close(console.log("database is closed"));
   }
