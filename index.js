7e846cbc89562b764de2a9ef45abe06a1d3b731b
Meraj Faizan <mdmaraj1500@gmail.com>
2023-12-22 09:00:56 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// connect to database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k5v5ibx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// api routes
+async function run() {
+  try {
+    // await client.connect();
+    const userCollection = client.db("taskDB").collection("users");
+
+    
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
+  } finally {
+    // await client.close(console.log("database is closed"));
+  }
+}
+run().catch((err) => console.log(err));
+
+// initial api routes and listen.
+app.get("/", (req, res) => {
+  res.send("task manager server is online");
+});
+
+app.listen(port, () => {
+  console.log(`task manager server listening on port ${port}`);
+});
